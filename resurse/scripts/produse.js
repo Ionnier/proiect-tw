3af199b46592b8448662e9ee3f9b332ca05ede52
Dragos Bahrim <dragosbahrim@outlook.com>
2021-12-06 10:40:36 +0200

    Tweak etapa 5

--- resurse/scripts/produse.js
+++ resurse/scripts/produse.js
@@ -35,46 +35,45 @@ function reset_range() {
     }
     var platforme = document.getElementsByClassName("platforma")
     var platforme_uniques = new Set()
-    for (let platforma of platforme){
+    for (let platforma of platforme) {
         platforme_uniques.add(platforma.innerHTML);
-        
     }
     var lista = document.getElementById("i_sel_multiplu");
-    for (let platforma of platforme_uniques){
+    for (let platforma of platforme_uniques) {
         var spanSuma;
         // <option value="zahar">zahar</option>
-            spanSuma = document.createElement("option");
-            spanSuma.innerHTML =platforma;
-            spanSuma.value = platforma;//<span id="..."
-            lista.appendChild(spanSuma); 
+        spanSuma = document.createElement("option");
+        spanSuma.innerHTML = platforma;
+        spanSuma.value = platforma;//<span id="..."
+        lista.appendChild(spanSuma);
     }
-    mediu = Math.round(greutate_total/articole.length)
-    usor = Math.round(mediu/2)
+    mediu = Math.round(greutate_total / articole.length)
+    usor = Math.round(mediu / 2)
     document.getElementById("i_sel_multiplu")
-    for(let el of document.getElementsByClassName("greutate-usor"))
-        el.innerHTML=usor
-    for(let el of document.getElementsByClassName("greutate-mediu"))
-        el.innerHTML=mediu
-    
+    for (let el of document.getElementsByClassName("greutate-usor"))
+        el.innerHTML = usor
+    for (let el of document.getElementsByClassName("greutate-mediu"))
+        el.innerHTML = mediu
+
 }
 
 window.onload = function () {
     var btn = document.getElementById("filtrare");
     btn.onclick = function () {
-        var optiuni=document.getElementById("i_sel_multiplu").options;		
-        var lista_op=[];
-        for(let opt of optiuni){
-            if(opt.selected)
+        var optiuni = document.getElementById("i_sel_multiplu").options;
+        var lista_op = [];
+        for (let opt of optiuni) {
+            if (opt.selected)
                 lista_op.push(opt.value)
         }
         var getTags = document.getElementById("tags").value.trim();
-            while (getTags != getTags.replace("  ", "")) {
-                getTags = getTags.replace("  ", "")
-            }
-            if (getTags[getTags.length - 1] == " ") {
-                getTags = getTags.slice(0, -1)
-            }
-            var tags = getTags.split(" ")
+        while (getTags != getTags.replace("  ", "")) {
+            getTags = getTags.replace("  ", "")
+        }
+        if (getTags[getTags.length - 1] == " ") {
+            getTags = getTags.slice(0, -1)
+        }
+        var tags = getTags.split(" ")
         var articole = document.getElementsByClassName("produs");
         for (let art of articole) {
 
@@ -106,7 +105,7 @@ window.onload = function () {
                 default: conditie3 = true;
             }
             var selCateg = document.getElementById("inp-categorie");
-            
+
             var conditie4 = (art.getElementsByClassName("val-categorie")[0].innerHTML.trim() == selCateg.value.trim() || selCateg.value == "toate");
 
             var conditie5 = !(tags.length == 0)
@@ -116,14 +115,14 @@ window.onload = function () {
                     continue;
                 }
                 let descr = art.getElementsByClassName("descriere")[0].innerHTML;
-                if (!(tag[0] != "+" || tag[0] != "-")) {
+                if ((tag[0] != "+" || tag[0] != "-")) {
                     var eroare_tags;
                     eroare_tags = document.getElementById("eroare_tags");
                     if (!eroare_tags) {
                         eroare_tags = document.createElement("eroare_tags");
                         eroare_tags.innerHTML = "<br>Eroare de formatare";//<span> Suma:...
                         eroare_tags.id = "eroare_tags";//<span id="..."
-                        document.getElementById("tags").insertAdjacentElement("afterend",eroare_tags);
+                        document.getElementById("tags").insertAdjacentElement("afterend", eroare_tags);
                         setTimeout(function () { document.getElementById("eroare_tags").remove() }, 2000);
                     }
                     results = []
@@ -140,13 +139,26 @@ window.onload = function () {
             }
             var conditie6 = true;
             results = []
-            if(lista_op.length>0){
+            if (lista_op.length > 0) {
                 var comp_list = art.getElementsByClassName("platforma")
-                for(let el of comp_list)
+                for (let el of comp_list)
                     results.push(lista_op.includes(el.innerHTML))
                 conditie6 = results.includes(true)
             }
-            if (conditie1 && conditie2 && conditie3 && conditie4 && conditie5 && conditie6)
+
+            var conditie7 = true;
+            var locatie = art.getElementsByClassName("locatie")[0].innerHTML.toLowerCase()
+            var checkboxes = document.getElementsByClassName("chckbox")
+            results = []
+            for (let checkbox of checkboxes) {
+                if (checkbox.checked == true) {
+                    if (locatie.includes(checkbox.name)) {
+                        results.push(true)
+                    }
+                }
+            }
+            conditie7 = results.includes(true)
+            if (conditie1 && conditie2 && conditie3 && conditie4 && conditie5 && conditie6 && conditie7)
                 art.style.display = "block";
 
         }
@@ -202,10 +214,13 @@ window.onload = function () {
         document.getElementById("i_rad4").checked = true;
         document.getElementById("inp-pret").value = document.getElementById("inp-pret").min;
         document.getElementById("infoRange").innerHTML = "(" + document.getElementById("inp-pret").min + ")";
-        document.getElementsByTagName("textarea")[0].value=""
-        document.getElementById("sel-toate").selected=true;
+        document.getElementsByTagName("textarea")[0].value = ""
+        document.getElementById("sel-toate").selected = true;
+        for (let elem of document.getElementsByClassName("chckbox")) {
+            elem.checked = true
+        }
         for (let el of document.getElementById("i_sel_multiplu").getElementsByTagName("option"))
-            el.selected=false
+            el.selected = false
 
         //de completat...
 
@@ -220,7 +235,6 @@ window.onload = function () {
     reset_range()
 }
 window.onkeydown = function (e) {
-    console.log(e);
     if (e.key == "c" && e.altKey == true) {
         var suma = 0;
         var articole = document.getElementsByClassName("produs");
@@ -233,7 +247,7 @@ window.onkeydown = function (e) {
         spanSuma = document.getElementById("numar-suma");
         if (!spanSuma) {
             spanSuma = document.createElement("span");
-            spanSuma.innerHTML = " Suma:" + suma;//<span> Suma:...
+            spanSuma.innerHTML = "Suma preturilor:" + suma;//<span> Suma:...
             spanSuma.id = "numar-suma";//<span id="..."
             document.getElementById("p-suma").appendChild(spanSuma);
             setTimeout(function () { document.getElementById("numar-suma").remove() }, 2000);
